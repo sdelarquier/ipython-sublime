b6fbdb6d3e3ecc92fe95ec3bccb71f5f51f22c96
Sebastien de Larquier <sebastien.delarquier@gmail.com>
2013-08-14 17:19:21 -0400

    first version: styling and javascript in good shape

---
+++ custom.js
@@ -0,0 +1,88 @@
+// leave at least 2 line with only a star on it below, or doc generation fails
+/**
+ *
+ *
+ * Placeholder for custom user javascript
+ * mainly to be overridden in profile/static/custom/custom.js
+ * This will always be an empty file in IPython
+ *
+ * User could add any javascript in the `profile/static/custom/custom.js` file
+ * (and should create it if it does not exist).
+ * It will be executed by the ipython notebook at load time.
+ *
+ * Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
+ *
+ * Example :
+ *
+ * Create a custom button in toolbar that execute `%qtconsole` in kernel
+ * and hence open a qtconsole attached to the same kernel as the current notebook
+ *
+ *    $([IPython.events]).on('notebook_loaded.Notebook', function(){
+ *        IPython.toolbar.add_buttons_group([
+ *            {
+ *                 'label'   : 'run qtconsole',
+ *                 'icon'    : 'ui-icon-calculator', // select your icon from http://jqueryui.com/themeroller/
+ *                 'callback': function(){IPython.notebook.kernel.execute('%qtconsole')}
+ *            }
+ *            // add more button here if needed.
+ *            ]);
+ *    });
+ *
+ * Example :
+ *
+ *  Use `jQuery.getScript(url [, success(script, textStatus, jqXHR)] );`
+ *  to load custom script into the notebook.
+ *
+ *    // to load the metadata ui extension example.
+ *    $.getScript('/static/notebook/js/celltoolbarpresets/example.js');
+ *    // or
+ *    // to load the metadata ui extension to control slideshow mode / reveal js for nbconvert
+ *    $.getScript('/static/notebook/js/celltoolbarpresets/slideshow.js');
+ *
+ *
+ * @module IPython
+ * @namespace IPython
+ * @class customjs
+ * @static
+ */
+
+$([IPython.events]).on('notebook_loaded.Notebook', function(){
+    IPython.toolbar.add_buttons_group([
+        {
+            'id'      : 'qt_b',
+            'label'   : 'Run QTconsole',
+            'icon'    : 'icon-code', // select your icon from http://jqueryui.com/themeroller/
+            'callback': function(){
+                IPython.notebook.kernel.execute('%qtconsole --colors=linux')
+                }
+        }
+    ]);
+    IPython.toolbar.add_buttons_group([
+        {
+            'id'      : 'kernel_int_b',
+            'label'   : 'Interrupt kernel',
+            'icon'    : 'icon-ban-circle', // select your icon from http://jqueryui.com/themeroller/
+            'callback': function(){
+                IPython.notebook.kernel.interrupt();
+                }
+        },
+        {
+            'id'      : 'kernel_restart_b',
+            'label'   : 'Restart kernel',
+            'icon'    : 'icon-refresh', // select your icon from http://jqueryui.com/themeroller/
+            'callback': function(){
+                IPython.notebook.restart_kernel();
+                }
+        },
+        {
+            'id'      : 'kill_b',
+            'label'   : 'Close and Halt',
+            'icon'    : 'icon-remove-sign', // select your icon from http://jqueryui.com/themeroller/
+            'callback': function(){
+                // IPython.notebook.save_checkpoint();
+                IPython.notebook.kernel.kill(); 
+                setTimeout(function(){window.close();}, 200);
+                }
+        }
+    ]);
+});
